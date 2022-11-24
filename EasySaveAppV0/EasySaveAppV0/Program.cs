cb464fd9cbfeef9f403f1e4a845b388e1827b3c3
Jean-Baptiste DELAMORT <jeanbaptiste.delamort@viacesi.fr>
2022-11-24 14:09:38 +0100

    Programme bilingue

--- EasySaveAppV0/EasySaveAppV0/Program.cs
+++ EasySaveAppV0/EasySaveAppV0/Program.cs
@@ -9,37 +9,37 @@ namespace EasySaveAppV0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("EasySave.v1 - Make your language choice");
+          //  Console.WriteLine("EasySave.v1 - Make your language choice");
             //LanguagechoiceFunction
             Choice();
         }
         public static void Choice()
         {
             FileEditing ObjfileEditing = new FileEditing();
-            Console.WriteLine("Easysave - make your choice\r\n 1.  Create a save \r\n 2.  Read the daily log \r\n 3.  Read daily saves states");
+            Console.WriteLine("Easysave - make your choice / Faites votre choix\r\n 1.  Create a save / Faire une sauvegarde \r\n 2.  Read the daily log / Voir les logs journaliers \r\n 3.  Read daily saves states / Voir l'état des sauvegardes");
             string menuChoice = Console.ReadLine();
             switch (menuChoice)
             {
                 case "1": //Creating a backup
-                    Console.WriteLine("Path that you want to copy (drop the file/folder)");
+                    Console.WriteLine("Path that you want to copy (drop the file/folder) / Chemin que vous souhaitez copier (déposez le fichier/dossier)");
                     string copypath = Console.ReadLine(); //String for the copied path
-                    Console.WriteLine("Path where to paste the copy (drop the file/folder)");
+                    Console.WriteLine("Path where to paste the copy (drop the file/folder) / Chemin où coller la copie (déposer le fichier/dossier)");
                     string pathpaste = Console.ReadLine(); //String for the pasted path
-                    Console.WriteLine("Choose the type of backup you wanna do:\r\n 1. Differential \r\n 2. Complete") ;
+                    Console.WriteLine("Choose the type of backup you wanna do / Choisissez le type de sauvegarde que vous voulez faire:\r\n 1. Differential / Différentiel \r\n 2. Complete / Complète") ;
                     string savechoice = Console.ReadLine();
                     if (savechoice=="1")
                     {
                         ObjfileEditing.DiffSave(copypath, pathpaste);
-                        Console.WriteLine("Differential save completed.");
+                        Console.WriteLine("Differential save completed / Sauvegarde différentiel terminé.");
                     }
                     else if(savechoice=="2")
                     {
                         ObjfileEditing.CompleteSave(copypath, pathpaste);
-                        Console.WriteLine("Complete Save completed.");
+                        Console.WriteLine("Complete save completed Sauvegarde complète terminé.");
                     }
                     else
                     {
-                        Console.WriteLine("Error. Please enter a valid choice.");
+                        Console.WriteLine("Error. Please enter a valid choice / Erreur. Veuillez entrer un choix valide.");
                     }
                     Console.ReadKey(); //awaiting input to leave
                     break;
@@ -49,7 +49,9 @@ namespace EasySaveAppV0
                 case "3": // Daily save state to print
                     break;
                 default:
-                    Console.WriteLine("Please enter a valid choice.");
+                    Console.WriteLine("Error. Please enter a valid choice / Erreur. Veuillez entrer un choix valide.\n");
+                    Program.Choice();
+
                     break;
             }
         }
