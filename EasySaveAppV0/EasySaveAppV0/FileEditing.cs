7249825586e0e750b40fdce3a8de335984d7475f
Valentin LAURENT <0valentinlaurent@gmail.com>
2022-11-25 10:34:48 +0100

    creating state
    Starting state creation - added logger path for file

--- EasySaveAppV0/EasySaveAppV0/FileEditing.cs
+++ EasySaveAppV0/EasySaveAppV0/FileEditing.cs
@@ -18,36 +18,30 @@ namespace EasySaveAppV0.Search
             this.copyDirectory = copyDirectory;
             this.pasteDirectory = pasteDirectory;
         }
-
         public void CompleteSave()
         {
             pasteDirectory += @"\" + name;
             //créer les dossiers
-            foreach (string dirPath in Directory.GetDirectories(copyDirectory, "*",
-                SearchOption.AllDirectories))
+            foreach (string dirPath in Directory.GetDirectories(copyDirectory, "*",SearchOption.AllDirectories))
                 Directory.CreateDirectory(dirPath.Replace(copyDirectory, pasteDirectory)); //créer le dossier dans la nouvelle sauvegarde pour chaque dossier existant
 
             //Copying all the files, replace if same name
-            foreach (string newPath in Directory.GetFiles(copyDirectory, "*.*",
-                SearchOption.AllDirectories))
+            foreach (string newPath in Directory.GetFiles(copyDirectory, "*.*",SearchOption.AllDirectories))
                 File.Copy(newPath, newPath.Replace(copyDirectory, pasteDirectory), true);
                 Logger Logg = new Logger();
                 Logg.SaveLog(copyDirectory, pasteDirectory, name);
-
         }
         public void DiffSave()
         {
             pasteDirectory += @"\" + name;
             //créer les dossiers
-            foreach (string dirPath in Directory.GetDirectories(copyDirectory, "*",
-                SearchOption.AllDirectories))
+            foreach (string dirPath in Directory.GetDirectories(copyDirectory, "*",SearchOption.AllDirectories))
                 if(Directory.GetLastAccessTime(dirPath)>Directory.GetLastAccessTime(copyDirectory))
                 { 
                     Directory.CreateDirectory(dirPath.Replace(copyDirectory, pasteDirectory));
                 }
-            //Copying all the files, replace if same name
-            foreach (string newPath in Directory.GetFiles(copyDirectory, "*.*",
-                SearchOption.AllDirectories))
+            //Copie les fichiers, remplace si nom identique
+            foreach (string newPath in Directory.GetFiles(copyDirectory, "*.*",SearchOption.AllDirectories))
                 if (File.GetLastAccessTime(newPath) > File.GetLastAccessTime(newPath.Replace(copyDirectory, pasteDirectory)))
                 { 
                     File.Copy(newPath, newPath.Replace(copyDirectory, pasteDirectory), true);
