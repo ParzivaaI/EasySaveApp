0ae0e49b5cb1dd87bb49724efa184e3f718a8b2a
ParzivaaI <93596247+ParzivaaI@users.noreply.github.com>
2022-11-30 12:57:26 +0100

    creating WPF

---
+++ EasySaveWPF/Model/model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasySaveWPF.View;
+
+namespace EasySaveWPF.Model
+{
+    internal class Model
+    {
+
+    }
+}
