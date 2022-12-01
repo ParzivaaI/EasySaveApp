7da724a57839728eecc5c47929909a2da774e2ba
OssamaAfri <ossama.afri@viacesi.fr>
2022-12-01 15:35:54 +0100

    MainView --> MainWindow.xaml

--- EasySaveWPF/Model/model.cs
+++ EasySaveWPF/Model/model.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EasySaveWPF.View;
+using EasySaveWPF.ViewModel;
 
 namespace EasySaveWPF.Model
 {
---
+++ EasySaveWPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasySaveWPF.Core;
+using EasySaveWPF.ViewModel;
+using EasySaveWPF.View;
+
+namespace EasySaveWPF.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public ViewModel MainVM { get; set; }
+
+        private Object _currentView;
+        public object CurrentView
+            
+           {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            MainVM = new ViewModel();
+            CurrentView = MainVM;
+        }
+    }
+}
