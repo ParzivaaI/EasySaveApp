2d8619d1e161fe967f560d09c90211db5aa456c8
Valentin LAURENT <0valentinlaurent@gmail.com>
2022-12-05 19:52:31 +0100

    Revert "fixing files"
    This reverts commit ee83e0979529961069cb03b64ce7e05931488907, reversing
    changes made to cfbb08272bfda7f62e79873ac8121cc522d76ff0.

--- EasySaveWPF/Core/RelayCommand.cs
+++ EasySaveWPF/Core/RelayCommand.cs
@@ -1,26 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace EasySaveWPF.ViewModel
+namespace EasySaveWPF.Core
 {
-    public class RelayCommand : ICommand
+    internal class RelayCommand : ICommand
     {
         private readonly Action<object> _execute;
-        private readonly Predicate<object> _canExecute;
+        private readonly Func<object, bool> _canExecute;
 
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
             _execute = execute;
             _canExecute = canExecute;
-        }
-        public event EventHandler CanExecuteChanged
-        {
-          add { CommandManager.RequerySuggested += value; }
-          remove { CommandManager.RequerySuggested -= value; }
         }
 
         public bool CanExecute(object parameter)
