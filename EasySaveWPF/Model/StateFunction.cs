9dbd219072422559c073c17df95df1c9b4562621
Valentin LAURENT <0valentinlaurent@gmail.com>
2022-12-05 15:34:14 +0100

    Update StateFunction.cs

--- EasySaveWPF/Model/StateFunction.cs
+++ EasySaveWPF/Model/StateFunction.cs
@@ -44,8 +44,7 @@ using System.Timers;
             this.FilePath = this.CurrentDirectory + "/" + this.FileName;
             this.FileLength = 0;
         }
-
-        public void StateCreate(string adresscopy, string adresspast, string FName,bool isActive, int filesLeft, long totalFileSize)
+        public void StateCreate(string adresscopy, string adresspast, string FName,bool isActive, int filesLeft, long totalFileSize, string savecompleted)
         {
                 this.FileLength = filesLeft;
                 using (System.IO.StreamWriter w = System.IO.File.AppendText(this.FilePath))
@@ -56,6 +55,7 @@ using System.Timers;
                     w.Write("Time : {0} {1} \n", DateTime.Now.ToShortDateString(),DateTime.Now.ToLongTimeString());
                     w.Write("FilesLeft : {0} \n", FileLength);
                     w.Write("Active: {0}\n",isActive);
+                    w.Write("Complete: {0}\n", savecompleted);
                     w.Write("Size: {0}\n", totalFileSize);
                     w.Write("\n");
                 }
