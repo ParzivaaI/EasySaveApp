698d9475bf3f4bcd1ede03d725e073358e4e97b7
OssamaAfri <ossama.afri@viacesi.fr>
2022-12-07 13:49:57 +0100

    Merge

--- EasySaveAppV0/EasySaveAppV0/Program.cs
+++ EasySaveAppV0/EasySaveAppV0/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
-
 using EasySaveAppV0.Search;
-using EasySaveAppV0.language;
-using EasySaveAppV0.log;
 
 namespace EasySaveAppV0
 {
@@ -18,34 +15,93 @@ namespace EasySaveAppV0
         public static void Choice()
         {
             FileEditing ObjfileEditing = new FileEditing();
-            Console.WriteLine("Easysave - make your choice / Faites votre choix\r\n 1.  Create a save / Faire une sauvegarde \r\n 2.  Read the daily log / Voir les logs journaliers \r\n 3.  Read daily saves states / Voir l'état des sauvegardes");
+            Console.WriteLine("Easysave - Choose a Language :\r\n 1. English \r\n 2. frensh ");
+
+            string ChoiceLanguage = Console.ReadLine();
+            if (ChoiceLanguage == "1")
+            { 
+            Console.WriteLine("Easysave - make your choice \r\n 1.  Create a save  \r\n 2.  Read the daily log  \r\n 3.  Read daily saves states");
+            }
+            else if (ChoiceLanguage == "2")
+            {
+               Console.WriteLine("Easysave - faites votre choix\r\n 1.Créer une sauvegarde \r\n 2.Lire les Logs \r\n 3.Lire les States");
+            }
             string menuChoice = Console.ReadLine();
             switch (menuChoice)
             {
                 case "1": //Creating a backup
-                    Console.WriteLine("Name of the folder/Nom du dossier");
+                    if (ChoiceLanguage == "1")
+                    {
+                        Console.WriteLine("Name of the folder");
+                    }
+                    else if (ChoiceLanguage == "2")
+                    {
+                        Console.WriteLine("Nom du dossier");
+                    }
                     string directoryName = Console.ReadLine();
-                    Console.WriteLine("Path that you want to copy (drop the file/folder) / Chemin que vous souhaitez copier (déposez le fichier/dossier)");
+                    if (ChoiceLanguage == "1")
+                    {
+                        Console.WriteLine("Path that you want to copy (drop the file/folder)");
+                    }
+                    else if (ChoiceLanguage == "2")
+                    {
+                        Console.WriteLine("Chemin que vous souhaitez copier (déposez le fichier/dossier)");
+                    }
                     string copyPath = Console.ReadLine(); //String for the copied path
-                    Console.WriteLine("Path where to paste the copy (drop the file/folder) / Chemin où coller la copie (déposer le fichier/dossier)");
+                    if (ChoiceLanguage == "1")
+                    {
+                        Console.WriteLine("Path where to paste the copy (drop the file/folder)");
+                    }
+                    else if (ChoiceLanguage == "2")
+                    {
+                        Console.WriteLine("Chemin où coller la copie (déposer le fichier/dossier)");
+                    }
                     string pathPaste = Console.ReadLine(); //String for the pasted path
-                    Console.WriteLine("Choose the type of backup you wanna do / Choisissez le type de sauvegarde que vous voulez faire:\r\n 1. Differential / Différentiel \r\n 2. Complete / Complète") ;
+                    if (ChoiceLanguage == "1")
+                    {
+                        Console.WriteLine("Choose the type of backup you wanna do:\r\n 1. Differential \r\n 2. Complete");
+                    }
+                    else if (ChoiceLanguage == "2")
+                    {
+                        Console.WriteLine("Choisissez le type de sauvegarde que vous voulez faire:\r\n 1. Différentiel \r\n 2. Complète");
+                    }
                     string saveChoice = Console.ReadLine();
                     int sizeofFiles = Directory.GetFiles(copyPath).Length;
                     ObjfileEditing.Variables(directoryName, copyPath, pathPaste, sizeofFiles);
                     if (saveChoice=="1")
                     {
                         ObjfileEditing.DiffSave();
-                        Console.WriteLine("Differential save completed / Sauvegarde différentiel terminé.");
+                        if (ChoiceLanguage == "1")
+                        {
+                            Console.WriteLine("Differential save completed ");
+                        }
+                        else if (ChoiceLanguage == "2")
+                        {
+                            Console.WriteLine("Sauvegarde différentiel terminé");
+                        }
                     }
                     else if(saveChoice=="2")
                     {
                         ObjfileEditing.CompleteSave();
-                        Console.WriteLine("Complete save completed Sauvegarde complète terminé.");
+                        if (ChoiceLanguage == "1")
+                        {
+                            Console.WriteLine("Complete save completed");
+                        }
+                        else if (ChoiceLanguage == "2")
+                        {
+                            Console.WriteLine("Sauvegarde complète terminé");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Error. Please enter a valid choice / Erreur. Veuillez entrer un choix valide.");
+                        if (ChoiceLanguage == "1")
+                        {
+                            Console.WriteLine("Error. Please enter a valid choice");
+                        }
+                        else if (ChoiceLanguage == "2")
+                        {
+                            Console.WriteLine("Erreur. Veuillez entrer un choix valide");
+                        }
                     }
                     Console.ReadKey(); //awaiting input to leave
                     break;
@@ -56,7 +112,14 @@ namespace EasySaveAppV0
                     Console.WriteLine(File.ReadAllText(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\" + DateTime.Now.ToString("dd-MM-yyyy") + " State.json"));
                     break;
                 default:
-                    Console.WriteLine("Error. Please enter a valid choice / Erreur. Veuillez entrer un choix valide.\n");
+                    if (ChoiceLanguage == "1")
+                    {
+                        Console.WriteLine("EError. Please enter a valid choice");
+                    }
+                    else if (ChoiceLanguage == "2")
+                    {
+                        Console.WriteLine("Erreur. Veuillez entrer un choix valide");
+                    }
                     Program.Choice();
                     break;
             }
